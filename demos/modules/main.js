c25d9ecb5c43ca37e568fb14dcb43b310247a8c9
John Paxton <pax@speedingplanet.com>
2021-02-01 12:13:48 -0500

    Monday lunch

--- demos/modules/main.js
+++ demos/modules/main.js
@@ -1,3 +1,9 @@
 // The main entry point for our JavaScript application.
+// import {greeting} from './utilities.js';
+import * as boris from './utilities.js';
+import {greeting as specialGreeting} from './utilities.js';
+
 
 console.log('Hello from a module.');
+boris.greeting();
+specialGreeting();
